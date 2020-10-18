e21ff795134342bab86215876f79ae4b963bf572
david barkhuizen <david.barkhuizen@gmail.com>
2020-10-18 12:51:07 +0200

    starting with Fluid module; Noka 6.1 test phone specs

---
+++ studio/README.md
@@ -0,0 +1,17 @@
+# Fluid Authentication & Authorization
+
+## Test Phone Specifications
+
+### Nokia 6.1
+
+label|text
+-----|----
+released|May 2018
+body dimensions|148.8 x 75.8 x 8.2 mm
+release OS|Android 8.1
+chipset|Qualcomm SDM630 Snapdragon 630 (14 nm)
+CPU|Octa-core 2.2 GHz Cortex-A53
+GPU|Adreno 508
+RAM|32GB 3GB RAM, 32GB 4GB RAM, 64GB 4GB RAM eMMC 5.1
+USB port|C
+NFC|yes
---
+++ studio/fluid/src/main/java/za/co/indrajala/fluid/Fluid.kt
@@ -0,0 +1,88 @@
+package za.co.indrajala.fluid
+
+import android.security.keystore.KeyGenParameterSpec
+import android.security.keystore.KeyProperties
+import android.util.Log
+import java.security.KeyPairGenerator
+import java.security.KeyStore
+
+
+class Fluid {
+
+    companion object {
+
+        val ENC_DEC = KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
+        val SIGN_VERIFY = KeyProperties.PURPOSE_SIGN or KeyProperties.PURPOSE_VERIFY
+
+        private const val ANDROID_LOG_TAG = "fluid.log"
+        private const val ROOT_KEY_ALIAS = "fluid.root"
+        private const val KEY_STORE_NAME = "fluid"
+
+        private val keystoreSecret: CharArray = "password".toCharArray()
+
+        private fun log(s: String) {
+            Log.v(ANDROID_LOG_TAG, s)
+        }
+
+        private fun log(label: String, text: String) {
+            log("$label: $text")
+        }
+
+        private fun log(label: String, number: Int) {
+            log("$label: $number")
+        }
+
+        private fun log(label: String, predicate: Boolean) {
+            log("$label: $predicate")
+        }
+    }
+
+    fun fingerprintDevice() {
+        val apiLevel = android.os.Build.VERSION.SDK_INT
+
+        log("device fingerprint")
+        log("android API level", apiLevel)
+    }
+
+    fun init() {
+        log("Fluid Authentication & Authorization from Indrajala")
+
+        fingerprintDevice()
+
+        val ks: KeyStore = KeyStore.getInstance("AndroidKeyStore")
+
+        log("KeyStore")
+        log("type", ks.type)
+        log("provider", ks.provider.name)
+        // JKS java key store (Oracle JDK)
+        // BKS bouncy-castle key store
+
+        ks.load(null)
+
+        val keyGenerator = KeyPairGenerator.getInstance(
+            KeyProperties.KEY_ALGORITHM_RSA, "AndroidKeyStore"
+        )
+
+        val keySpec = KeyGenParameterSpec.Builder(ROOT_KEY_ALIAS, SIGN_VERIFY)
+            .setDigests(KeyProperties.DIGEST_SHA256, KeyProperties.DIGEST_SHA512)
+            .setKeySize(4096)
+            .build()
+
+        keyGenerator.initialize(keySpec)
+
+        val keyPair = keyGenerator.generateKeyPair()
+
+        log("isStrongBoxBacked", keySpec.isStrongBoxBacked)
+        
+
+
+
+
+
+//        val x = keyPair.is
+
+
+
+
+    }
+}
