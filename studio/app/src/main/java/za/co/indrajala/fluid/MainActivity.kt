276467696dd7097d176438900ffe6c2d3b3155eb
david barkhuizen <david.barkhuizen@gmail.com>
2020-10-19 11:07:16 +0200

    some structure; notes on Android KeyStore

--- studio/app/src/main/java/za/co/indrajala/fluid/MainActivity.kt
+++ studio/app/src/main/java/za/co/indrajala/fluid/MainActivity.kt
@@ -2,12 +2,17 @@ package za.co.indrajala.fluid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import za.co.indrajala.fluid.util.log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Fluid().init()
+        try {
+            val fluid = Fluid().init()
+        } catch (e: Exception) {
+            log.v(e.toString())
+        }
     }
 }
--- studio/fluid/src/main/java/za/co/indrajala/fluid/Fluid.kt
+++ studio/fluid/src/main/java/za/co/indrajala/fluid/Fluid.kt
@@ -13,44 +13,56 @@ class Fluid {
 
     companion object {
 
+        private const val ANDROID_KEY_STORE_NAME = "AndroidKeyStore"
+
         private const val ROOT_KEY_ALIAS = "fluid.root"
         private const val KEY_STORE_NAME = "fluid"
 
         private val keystoreSecret: CharArray = "password".toCharArray()
     }
 
-    private fun deviceFingerprint() {
+    private var initialized = false
+    private val ks: KeyStore = KeyStore.getInstance(ANDROID_KEY_STORE_NAME)
+
+    private fun fingerprintDevice() {
         val apiLevel = android.os.Build.VERSION.SDK_INT
 
         log.v("device fingerprint")
-        log.v("android API level", apiLevel)
+        log.v("Android API level", apiLevel)
     }
 
-    fun init() {
-        log.v("Fluid Authentication & Authorization from Indrajala")
+    private fun initializeKeystore() {
 
-        deviceFingerprint()
+        log.v("KeyStore", "Type ${ks.type}, Provider ${ks.provider.name}")
 
-        val ks: KeyStore = KeyStore.getInstance("AndroidKeyStore")
-
-        log.v("KeyStore")
-        log.v("type", ks.type)
-        log.v("provider", ks.provider.name)
+        // providers
         // JKS java key store (Oracle JDK)
         // BKS bouncy-castle key store
 
         ks.load(null)
+    }
+
+    private fun generateDeviceRootKey() {
+
+        log.v("generating asymmetric RSA key...")
+
+        val alias = ROOT_KEY_ALIAS
+        val keySize = 2048
+        val usage = KeyPurpose.Integrity.ordinal
+        val digest = KeyProperties.DIGEST_SHA512
 
         val keyGenerator = KeyPairGenerator.getInstance(
-            KeyProperties.KEY_ALGORITHM_RSA, "AndroidKeyStore"
+            KeyProperties.KEY_ALGORITHM_RSA, ANDROID_KEY_STORE_NAME
         )
 
-        val keySpec = KeyGenParameterSpec.Builder(ROOT_KEY_ALIAS, KeyPurpose.Integrity.ordinal)
-            .setDigests(KeyProperties.DIGEST_SHA256, KeyProperties.DIGEST_SHA512)
-            .setKeySize(4096)
+        val keySpec = KeyGenParameterSpec
+            .Builder(alias, usage)
+            .setDigests(digest)
+            .setKeySize(keySize) // 4096 => not supported by low power key operation suite
+            //.setIsStrongBoxBacked(true) => android.security.keystore.StrongBoxUnavailableException: Failed to generate key pair
             .build()
 
-        log.v("KeySpec.isStrongBoxBacked", keySpec.isStrongBoxBacked)
+        // log.v("KeySpec.isStrongBoxBacked", keySpec.isStrongBoxBacked)
 
         keyGenerator.initialize(keySpec)
 
@@ -60,11 +72,28 @@ class Fluid {
         val privateKey = (entry as KeyStore.PrivateKeyEntry).privateKey
         val publicKey = ks.getCertificate(ROOT_KEY_ALIAS).publicKey
 
-
-        val factory = KeyFactory.getInstance(privateKey.algorithm, "AndroidKeyStore")
+        val factory = KeyFactory.getInstance(privateKey.algorithm, ANDROID_KEY_STORE_NAME)
         val keyInfo = factory.getKeySpec(privateKey, KeyInfo::class.java)
 
         val isInsideSecureHardware: Boolean = keyInfo.isInsideSecureHardware
         log.v("PvtKey.KeyInfo.isInsideSecureHardware", isInsideSecureHardware)
+    }
+
+    fun init(): Fluid {
+        log.v("Fluid Integrity & Confidentiality - (C) 2020, Indrajala (Pty) Ltd")
+
+        if (initialized) {
+            log.v("already initialized")
+            return this
+        }
+
+        log.v("initializing...")
+
+        fingerprintDevice()
+        initializeKeystore()
+
+        generateDeviceRootKey()
+
+        return this
     }
 }
